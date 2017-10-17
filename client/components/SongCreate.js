ef914ba9479c2f935b8b39208f7b900986bcd2eb
tonyrud <threefloyds16>
2017-10-17 16:48:26 -0400

    refetching data on song creation

--- client/components/SongCreate.js
+++ client/components/SongCreate.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react'
 import gql from 'graphql-tag'
 import { graphql } from 'react-apollo'
+import { Link, hashHistory } from 'react-router'
+
+import query from './../queries/fetchSongs'
 
 class SongCreate extends Component {
   constructor(props) {
@@ -12,17 +15,24 @@ class SongCreate extends Component {
   onSubmit(event) {
     event.preventDefault()
 
-    this.props.mutate({
-      variables: {
-        title: this.state.title
-      }
-    })
-
+    this.props
+      .mutate({
+        variables: {
+          title: this.state.title,
+        },
+        refetchQueries: [
+          {
+            query,
+          },
+        ],
+      })
+      .then(data => hashHistory.push('/'))
   }
 
   render() {
     return (
       <div>
+        <Link to="/">Back</Link>
         <h3>Create a New Song</h3>
         <form onSubmit={this.onSubmit.bind(this)}>
           <label>Song Title:</label>
@@ -37,12 +47,12 @@ class SongCreate extends Component {
 }
 
 const mutation = gql`
-mutation AddSong($title: String){
-  addSong (title: $title) {
-    id
-    title
+  mutation AddSong($title: String) {
+    addSong(title: $title) {
+      id
+      title
+    }
   }
-}
 `
 
 export default graphql(mutation)(SongCreate)
--- client/components/SongList.js
+++ client/components/SongList.js
@@ -1,27 +1,30 @@
 import React, { Component } from 'react'
 import gql from 'graphql-tag'
 import { graphql } from 'react-apollo'
+import { Link } from 'react-router'
+
+import query from './../queries/fetchSongs'
 
 class SongList extends Component {
   renderSongs() {
     return this.props.data.songs.map(song => (
-      <li key={song.id} className="collection-item"> {song.title} </li>
+      <li key={song.id} className="collection-item">
+        {song.title}
+      </li>
     ))
   }
 
   render() {
     if (this.props.data.loading) return <div>Loading...</div>
-    return <div className="collection">{this.renderSongs()}</div>
+    return (
+      <div>
+        <ul className="collection">{this.renderSongs()}</ul>
+        <Link to="/songs/new" className="btn-floating btn-large red right">
+          <i className="material-icons">add</i>
+        </Link>
+      </div>
+    )
   }
 }
-
-const query = gql`
-  {
-    songs {
-      id
-      title
-    }
-  }
-`
 
 export default graphql(query)(SongList)
