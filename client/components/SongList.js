c0bd52fd94fb29d47ca322c5a567924f969e21a2
tonyrud <threefloyds16>
2017-10-17 15:37:52 -0400

    song create setup

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react'
+import gql from 'graphql-tag'
+import { graphql } from 'react-apollo'
 
-export default class SongList extends Component {
+class SongList extends Component {
+  renderSongs() {
+    return this.props.data.songs.map(song => (
+      <li key={song.id} className="collection-item"> {song.title} </li>
+    ))
+  }
+
   render() {
-    return (
-      <div>
-        SongList
-      </div>
-    )
+    if (this.props.data.loading) return <div>Loading...</div>
+    return <div className="collection">{this.renderSongs()}</div>
   }
 }
+
+const query = gql`
+  {
+    songs {
+      id
+      title
+    }
+  }
+`
+
+export default graphql(query)(SongList)
